454c0dfbf14ff5cec1c8564e8323171ce1e7eb58
Mano Sriram <mano.sriram0@gmail.com>
2025-09-03 00:52:46 +0530

    feat: Add DAP

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      -- Setup dap-ui
+      dapui.setup()
+
+
+
+      -- Auto open/close dap-ui
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      -- Key mappings
+      vim.keymap.set('n', '<Leader>dc', function() dap.continue() end, { desc = "Debug: Continue" })
+      vim.keymap.set('n', '<Leader>so', function() dap.step_over() end, { desc = "Debug: Step Over" })
+      vim.keymap.set('n', '<Leader>si', function() dap.step_into() end, { desc = "Debug: Step Into" })
+      vim.keymap.set('n', '<Leader>sO', function() dap.step_out() end, { desc = "Debug: Step Out" })
+      vim.keymap.set('n', '<Leader>db', function() dap.toggle_breakpoint() end, { desc = "Debug: Toggle Breakpoint" })
+      vim.keymap.set('n', '<Leader>dB', function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, { desc = "Debug: Set Conditional Breakpoint" })
+      vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end, { desc = "Debug: Open REPL" })
+      vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end, { desc = "Debug: Run Last" })
+      vim.keymap.set('n', '<Leader>du', function() dapui.toggle() end, { desc = "Debug: Toggle UI" })
+
+			dap.adapters.codelldb = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = "codelldb",
+					args = { "--port", "${port}" },
+				},
+			}
+
+			-- setup a debugger for zig projects
+			dap.configurations.zig = {
+				{
+					name = 'Launch',
+					type = 'codelldb',
+					request = 'launch',
+					program = '${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}',
+					cwd = '${workspaceFolder}',
+					stopOnEntry = false,
+					args = {},
+				},
+			}
+		end,
+	},
+}
