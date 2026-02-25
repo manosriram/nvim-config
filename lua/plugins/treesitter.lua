484b652c21fbf5f6ddcac3d28ad507f1497d8f2a
Mano Sriram <mano.sriram0@gmail.com>
2026-02-25 19:34:29 +0530

    Update leap.lua to use new codeberg url

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,18 @@
 return {
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "rust", "c", "lua", "vim", "vimdoc", "go", "v" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    -- Some versions moved things around. 
+    -- This check ensures the module exists before calling it.
+    local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not status_ok then
+        return
     end
+
+    configs.setup({
+      ensure_installed = { "c", "python", "rust", "go", "zig"},
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
 }
